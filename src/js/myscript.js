22af5ae3bce643d8c7ab54243c41e616e98f5315
megan adams <megan.adams@ge.com>
2022-06-06 17:46:58 -0400

    [feature] - fetch data and inject into HTML

--- src/js/myscript.js
+++ src/js/myscript.js
@@ -3,5 +3,35 @@ $(document).ready(function() {
     getRemoteData();
 });
 function getRemoteData() {
-    // write your code here to fetch data from api.
+
+    function displayData(data){
+        idTag = document.querySelector('.id')
+        countryTag = document.querySelector('.country')
+        continentTag = document.querySelector('.continent')
+        capitalTag = document.querySelector('.capital')
+        modifiedTag = document.querySelector('.modified')
+        data.map(item => {
+            idTag.appendChild(document.createElement('p'))
+            idTag.lastChild.innerHTML += item.id
+            countryTag.appendChild(document.createElement('p'))
+            countryTag.lastChild.innerHTML += item.countryName
+            continentTag.appendChild(document.createElement('p'))
+            continentTag.lastChild.innerHTML += item.continent
+            capitalTag.appendChild(document.createElement('p'))
+            capitalTag.lastChild.innerHTML += item.capital
+            modifiedTag.appendChild(document.createElement('p'))
+            modifiedTag.lastChild.innerHTML += item.modifiedOn
+
+        })
+    }
+    const URL = 'https://www.justcode.com/dataservice/api/country/list';
+    $.ajax({
+        url: URL,
+        type: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            displayData(data);
+        }
+    });
+    
 }
